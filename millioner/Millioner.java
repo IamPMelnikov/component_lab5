6a79469ac4383f87cc3c5108e446e6e4da8dd2e4
IamPMelnikov <pav3luchenik@yandex.ru>
2023-04-16 23:40:04 +0500

    java

---
+++ millioner/Millioner.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.millioner;
+
+/**
+ *
+ * @author user
+ */
+public class Millioner {
+
+    public static void main(String[] args) {
+        GameForm gf = new GameForm();
+        gf.setVisible(true);
+        
+    }
+}
